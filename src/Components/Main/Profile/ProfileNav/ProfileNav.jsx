8ee5232d2f2881c270c52c10f3a2fe51fba60d6e
Ivanna Pokhyl <ivanna.pokhyl@gmail.com>
2022-10-21 14:54:35 +0300

    Create Profile Nav

---
+++ src/Components/Main/Profile/ProfileNav/ProfileNav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from './ProfileNav.module.css'
+import {NavLink} from "react-router-dom";
+
+const ProfileNav = () => {
+    return (
+        <div className={style.wrapper}>
+            <NavLink to='timeline/' className={style.navLink}><div className={style.navItem}>Timeline</div></NavLink>
+            <NavLink to='about/' className={style.navLink}><div className={style.navItem}>About</div></NavLink>
+            <NavLink to='friends/' className={style.navLink}><div className={style.navItem}>Friends</div></NavLink>
+            <NavLink to='photos/' className={style.navLink}><div className={style.navItem}>Photos</div></NavLink>
+        </div>
+)
+}
+
+export default ProfileNav;
